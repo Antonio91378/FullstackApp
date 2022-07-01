da2deb847eb638e3dae18d872faac70dad7df665
Antonio Dias <antonio.dias@bit.com.br>
2022-07-01 17:06:51 -0300

    <FEAT> : implementando variaveis env :)

--- back-end/index.js
+++ back-end/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -31,7 +32,7 @@ Frankbob12345!
 //espera conectar no banco de dados antes de liberar o endPoint
 mongoose
   .connect(
-    "mongodb+srv://espartacos91378:Frankbob12345!@dbformyapi.tyy2l.mongodb.net/?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@dbformyapi.tyy2l.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() => {
     console.log("Conectamos ao mongoDB!");
